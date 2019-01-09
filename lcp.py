69a416b266a69227160ce664c88383c475110146
小嶋泰弘 <kojimayasuhiro@keisanki.local>
2019-01-09 14:00:24 -0500

    Prameter samplers are implemented

--- lcp.py
+++ lcp.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+import math
 
 
 class DataManager:
@@ -12,8 +14,8 @@ class DataManager:
 
     def set_time(self, t):
         selected_df = self.df.loc[self.df.t == t]
-        self.r = selected_df[["x", "y", "z"]]
-        self.v = selected_df[["vx", "vy", "vz"]]
+        self.r = selected_df[["x", "y", "z"]].values
+        self.v = selected_df[["vx", "vy", "vz"]].values
 
 
 class Decider:
@@ -21,18 +23,14 @@ class Decider:
     This class calculate difference between simulation and real data,
     and decide whether simulation results is acceptable.
     """
-    def __init__(self, length, min_eps=1.0e-4, order=2):
+    def __init__(self, length, v, min_eps=1.0e-4, order=2):
         """
         Initialize epx vec, which represents accetance threshold for each round
+        Aquire the real data velocity used in evaluation
         Waring:: Eps vec should be considered later
         """
         self.eps_vec = min_eps * np.arange(length, 1)**order
-
-    def load_data(self, dm):
-        """
-        Aquire the real data velocity used in evaluation
-        """
-        self.v = dm.v
+        self.v = v
 
     def decide(self, v, t):
         """
@@ -88,4 +86,98 @@ class SampleRegister:
         return(
             np.random.choice(
                 self.theta_vec,
-                size=1, p=self.w_vec/np.sum(self.w_vec)))
+                size=1, p=self.w_vec/np.sum(self.w_vec))[0])
+
+
+class PriorSampler:
+    """
+    Sample parameters from prior distribution
+    """
+    def __init__(self, theta_key_vec, boundary_dict, K_dict):
+        self.theta_key_vec = theta_key_vec
+        self.boundary_dict = boundary_dict
+        self.K_dict = K_dict
+        point_num = K_dict[theta_key_vec[0]].shape[0]
+        self.point_num_vec = np.full(point_num, 1.0)
+
+    def sample_param(self):
+        """
+        Sample parameters based on prior distribution
+        Mean of each parameter sampled from uniform distribution
+        Local values are sampled from multivariate gaussian,
+        which reflect spatial location of each cell
+        """
+        theta = {}
+        for theta_key in self.theta_key_vec:
+            theta[theta_key] = {}
+            theta[theta_key]["mean"] = np.random.uniform(
+                self.boundary_dict[theta_key][0],
+                self.boundary_dict[theta_key][1],
+                size=1)[0]
+            # loca parameters sampled from mean
+            theta[theta_key]["local"] = np.random.multivariate_normal(
+                theta[theta_key]["mean"] * self.point_num_vec,
+                self.K_dict[theta_key])
+        return(theta)
+
+    def calc_prior_prob(self, theta):
+        """
+        Calculate the likelihood of theta based on prior distribution
+        """
+        prob = 1
+        for theta_key in self.theta_key_vec:
+            interval = self.boundary_dict[theta_key][1] - self.boundary_dict[theta_key][0]
+            mean_prob = 1.0/interval
+            local_prob = stats.multivariate_normal(
+                theta[theta_key]["mean"] * self.point_num_vec,
+                self.K_dict[theta_key]).pdf(theta[theta_key]["local"])
+            prob *= mean_prob * local_prob
+        return(prob)
+
+
+class SequentialSampler(PriorSampler):
+    """
+    Parameter sampling based on previous parameter
+    """
+    def __init__(self, theta_key_vec, var_dict, K_dict):
+        self.theta_key_vec = theta_key_vec
+        self.var_dict = var_dict
+        self.K_dict = K_dict
+        point_num = K_dict[theta_key_vec[0]].shape[0]
+        self.point_num_vec = np.full(point_num, 1.0)
+
+    def sample_param(self, pre_theta):
+        """
+        Sample parameters from previous theta
+        """
+        theta = {}
+        for theta_key in self.theta_key_vec:
+            theta[theta_key] = {}
+            # mean parameter sampled from previous mean
+            theta[theta_key]["mean"] = np.random.normal(
+                pre_theta[theta_key]["mean"],
+                self.var_dict[theta_key],
+                size=1)[0]
+            # local parameters sampled from previous local
+            theta[theta_key]["local"] = np.random.multivariate_normal(
+                pre_theta[theta_key]["local"],
+                self.K_dict[theta_key])
+        return(theta)
+
+    def calc_transition_prob(self, pre_theta, theta):
+        """
+        Calculate transition probability from pre_theta to theta
+        """
+        prob = 1
+        for theta_key in self.theta_key_vec:
+            # mean parameter sampled from previous mean
+            mean_prob = stats.norm.pdf(
+                theta[theta_key]["mean"],
+                loc=pre_theta[theta_key]["mean"],
+                scale=math.sqrt(self.var_dict[theta_key]))
+            # local parameters sampled from previous local
+            local_prob = stats.multivariate_normal(
+                pre_theta[theta_key]["local"],
+                self.K_dict[theta_key]).pdf(theta[theta_key]["local"])
+            prob *= mean_prob * local_prob
+        return(prob)
--- test_lcp.py
+++ test_lcp.py
@@ -1,9 +1,9 @@
-from lcp import DataManager, Decider, SampleRegister
+from lcp import DataManager, Decider, SampleRegister, PriorSampler, SequentialSampler
 import pandas as pd
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
 import numpy as np
-import unittest
+from unittest.mock import MagicMock
 
 
 class TestDataManager(object):
@@ -28,7 +28,7 @@ class TestDataManager(object):
 
 class TestDecider(object):
     def test_decide(self):
-        dc = Decider(10)
+        dc = Decider(10, 1)
         dc.eps_vec = np.array([3, 2, 1])
         dc.v = np.array([0, 0])
         v1 = np.array([1.5, 0])
@@ -49,3 +49,97 @@ class TestSampleRegister(object):
         sr.register_new(new_theta, 1)
         assert_array_equal(sr.new_theta_vec, np.array([1, 5]))
         assert_array_equal(sr.new_w_vec, np.array([5, 5/7]))
+
+    def test_load_new(self):
+        sr = SampleRegister(1, 1)
+        sr.w_vec = np.array([0, 1])
+        sr.theta_vec = np.array([1, 0])
+        assert sr.choice() == 0
+
+
+class TestPriorSampler(object):
+    def test_sample_param(self):
+        theta_key_vec = np.array(["a", "b"])
+        boundary_dict = {
+            "a": (0, 1),
+            "b": (90, 100)
+        }
+        K_dict = {
+            "a": np.array([[1, 0], [0, 1]]),
+            "b": np.array([[1, 0], [0, 1]])
+        }
+        ps = PriorSampler(theta_key_vec, boundary_dict, K_dict)
+        theta = ps.sample_param()
+        assert theta.keys() == boundary_dict.keys()
+        assert theta["a"]["local"].shape == (2,)
+        assert theta["b"]["mean"] > 80
+        assert theta["a"]["mean"] < 1.1
+
+    def test_calc_prior_prob(self):
+        theta_key_vec = np.array(["a", "b"])
+        boundary_dict = {
+            "a": (0, 1),
+            "b": (90, 100)
+        }
+        K_dict = {
+            "a": np.array([[1, 0], [0, 1]]),
+            "b": np.array([[1, 0], [0, 1]])
+        }
+        ps = PriorSampler(theta_key_vec, boundary_dict, K_dict)
+        theta = ps.sample_param()
+        prior_prob = ps.calc_prior_prob(theta)
+        assert prior_prob > 0
+        assert prior_prob < 1
+
+
+class TestSequentialSampler(object):
+    def test_sample_param(self):
+        theta_key_vec = np.array(["a", "b"])
+        var_dict = {
+            "a": 1,
+            "b": 1
+        }
+        K_dict = {
+            "a": np.array([[1, 0], [0, 1]]),
+            "b": np.array([[1, 0], [0, 1]])
+        }
+        pre_theta = {
+            "a": {
+                "mean": 2,
+                "local": np.array([0, 1])
+            },
+            "b":  {
+                "mean": 2,
+                "local": np.array([0, 1])
+            }
+        }
+        ss = SequentialSampler(theta_key_vec, var_dict, K_dict)
+        theta = ss.sample_param(pre_theta)
+        assert theta.keys() == var_dict.keys()
+        assert theta["a"]["local"].shape == (2,)
+
+    def test_calc_prior_prob(self):
+        theta_key_vec = np.array(["a", "b"])
+        var_dict = {
+            "a": 1,
+            "b": 1
+        }
+        K_dict = {
+            "a": np.array([[1, 0], [0, 1]]),
+            "b": np.array([[1, 0], [0, 1]])
+        }
+        pre_theta = {
+            "a": {
+                "mean": 2,
+                "local": np.array([0, 1])
+            },
+            "b":  {
+                "mean": 2,
+                "local": np.array([0, 1])
+            }
+        }
+        ss = SequentialSampler(theta_key_vec, var_dict, K_dict)
+        theta = ss.sample_param(pre_theta)
+        transition_prob = ss.calc_transition_prob(pre_theta, theta)
+        assert transition_prob > 0
+        assert transition_prob < 1
